97b81dd966f008000353c0fd9b534bec4ef22e35
Surkky <cruz.gcp@gmail.com>
2024-05-13 18:56:05 +0200

    Created SearchCards class and initalice atributes

---
+++ src/main/java/cardshop/finalprojectcardshop/SearchCards.java
@@ -0,0 +1,10 @@
+package cardshop.finalprojectcardshop;
+
+public class SearchCards
+{
+    private String name;
+    private String collection;
+    private String rarity;
+    private String description;
+    private int quantity;
+}
