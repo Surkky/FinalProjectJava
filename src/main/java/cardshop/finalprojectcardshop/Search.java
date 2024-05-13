e4b44afc64e82aaf53ca338933591b79b54df1be
Surkky <cruz.gcp@gmail.com>
2024-05-13 18:36:01 +0200

    Add Search id's

--- src/main/java/cardshop/finalprojectcardshop/Search.java
+++ src/main/java/cardshop/finalprojectcardshop/Search.java
@@ -1,4 +1,23 @@
 package cardshop.finalprojectcardshop;
 
-public class Search {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class Search
+{
+
+    @FXML
+    private Button searchCollection;
+    @FXML
+    private Button searchName;
+    @FXML
+    private TextField CollectionText;
+    @FXML
+    private TextField NameText;
+    @FXML
+    private Button exitButton;
+    @FXML
+    private TableView chartCards;
 }
