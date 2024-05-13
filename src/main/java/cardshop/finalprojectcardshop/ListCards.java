b5a162a4b60eb85da0ecf572929347c4904d12dc
Surkky <cruz.gcp@gmail.com>
2024-05-13 18:35:45 +0200

    Add ListCards id's

--- src/main/java/cardshop/finalprojectcardshop/ListCards.java
+++ src/main/java/cardshop/finalprojectcardshop/ListCards.java
@@ -1,4 +1,22 @@
 package cardshop.finalprojectcardshop;
 
-public class ListCards {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
+public class ListCards
+{
+
+    @FXML
+    private TableColumn columnListQuantity;
+    @FXML
+    private TableColumn columnListDescription;
+    @FXML
+    private TableColumn columnListRarity;
+    @FXML
+    private TableColumn columnListCollection;
+    @FXML
+    private Button exitButton;
+    @FXML
+    private TableColumn columnListCardName;
 }
